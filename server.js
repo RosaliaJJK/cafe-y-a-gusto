b30dda1b5f46db50cbd01dff7443e55c54a0b28b
RosaliaJJK <pruebaboot117@gmail.com>
2026-05-17 01:06:34 -0600

    Fix conexion mysql

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ app.use(express.static(__dirname));
 const db = mysql.createConnection({
 
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 
 });
+
+console.log("Servidor iniciando...");
 
 db.connect((err) => {
 
